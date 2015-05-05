fe3aa90050374a55f303571a25694db0f716e386
Martin Bačovský <martin.bacovsky@gmail.com>
2015-05-05 22:34:06 +0200

    Fixes #10386 - Use extended error handling from Foreman plugin

--- lib/hammer_cli_foreman_docker.rb
+++ lib/hammer_cli_foreman_docker.rb
@@ -1,6 +1,10 @@
 require 'hammer_cli_foreman'
 
 module HammerCLIForemanDocker
+  def self.exception_handler_class
+    HammerCLIForeman::ExceptionHandler
+  end
+
   require 'hammer_cli_foreman_docker/i18n'
 
   HammerCLI::MainCommand.lazy_subcommand("docker", _("Manipulate docker content"),
